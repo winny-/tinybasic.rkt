dae05174341f31d514cc822a7ca81c138791656f
Winston Weinert <git@winny.tech>
2022-07-29 09:29:16 -0500

    examples: add quine

---
+++ tinybasic-examples/examples/quine.rkt
@@ -0,0 +1,6 @@
+#lang tinybasic
+100 REM Turns out one can make any BASIC program a quine through the LIST
+120 REM statement.  Who knew!  To address the hashlang line, one can PRINT it.
+140 PRINT "#lang tinybasic"
+160 LIST
+180 END
